b985cf08ac8bbf1f23c8608a4c1e3b9be314572b
qmya <qandeelgul90@gmail.com>
2021-02-02 09:12:39 -0500

    fetch the data from the URL

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,33 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async function () {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+    //always we convert the response into json
+    const data = await res.json();
+    //if the id is not right than throw a new error
+    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+    console.log(res, data);
+
+    //let create a new recipe object to remove the underscore from the name
+    //let recipe = data.data.recipe; //destructure it
+    let { recipe } = data.data;
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+    };
+    console.log(recipe);
+  } catch (error) {
+    console.error(error);
+  }
+};
+showRecipe();
