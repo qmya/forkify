38c2111ac4d69c056b2548b9e2c6712d53e1fa06
qmya <qandeelgul90@gmail.com>
2021-02-04 13:33:47 -0500

    changes the # to _

--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,23 +1,23 @@
 import icons from 'url:../../img/icons.svg'; //parcel 2 if its an image you have to use url: with it
 import { Fraction } from 'fractional';
-console.log(Fraction);
 
 class RecipeView {
   //We want each view have a couple of private methods
   //So classes makes this so easy to implement the private methods
 
-  #parentElement = document.querySelector('.recipe');
-  #data;
+  _parentElement = document.querySelector('.recipe');
+  _data;
+
   render(data) {
-    this.#data = data;
+    this._data = data;
     //render is responsible for rendering HTML:
-    const markup = this.#generateMarkup();
-    this.#clear();
-    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+    const markup = this._generateMarkup();
+    this._clear();
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
   }
 
-  #clear() {
-    this.#parentElement.innerHTML = '';
+  _clear() {
+    this._parentElement.innerHTML = '';
   }
 
   renderSpinner = function () {
@@ -28,18 +28,18 @@ class RecipeView {
       </svg>
     </div>
     `;
-    this.#clear;
-    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+    this._clear;
+    this._parentElement.insertAdjacentHTML('afterbegin', markup);
   };
 
-  #generateMarkup() {
+  _generateMarkup() {
     return `
       <figure class="recipe__fig">
-        <img src="${this.#data.image}" alt="${
-      this.#data.title
+        <img src="${this._data.image}" alt="${
+      this._data.title
     }" class="recipe__img" />
         <h1 class="recipe__title">
-          <span>${this.#data.title}</span>
+          <span>${this._data.title}</span>
         </h1>
       </figure>
 
@@ -49,7 +49,7 @@ class RecipeView {
             <use href="${icons}#icon-clock"></use>
           </svg>
           <span class="recipe__info-data recipe__info-data--minutes">${
-            this.#data.cookingTime
+            this._data.cookingTime
           }</span>
           <span class="recipe__info-text">minutes</span>
         </div>
@@ -59,7 +59,7 @@ class RecipeView {
           <use href="${icons}#icon-users"></use>
         </svg>
         <span class="recipe__info-data recipe__info-data--people">${
-          this.#data.servings
+          this._data.servings
         }</span>
         <span class="recipe__info-text">servings</span>
 
@@ -92,7 +92,7 @@ class RecipeView {
       <div class="recipe__ingredients">
         <h2 class="heading--2">Recipe ingredients</h2>
         <ul class="recipe__ingredient-list">
-          ${this.#data.ingredients.map(this.#generateMarkupIngredient).join('')}
+          ${this._data.ingredients.map(this._generateMarkupIngredient).join('')}
       </div>
 
     <div class="recipe__directions">
@@ -100,13 +100,13 @@ class RecipeView {
       <p class="recipe__directions-text">
         This recipe was carefully designed and tested by
         <span class="recipe__publisher">
-        ${this.#data.publisher}
+        ${this._data.publisher}
         </span>. Please check out
         directions at their website.
       </p>
       <a
         class="btn--small recipe__btn"
-        href="${this.#data.sourceUrl}"
+        href="${this._data.sourceUrl}"
         target="_blank"
       >
         <span>Directions</span>
@@ -117,7 +117,7 @@ class RecipeView {
     </div>
   `;
   }
-  #generateMarkupIngredient = ing => {
+  _generateMarkupIngredient = ing => {
     return `
     <li class="recipe__ingredient">
       <svg class="recipe__icon">
